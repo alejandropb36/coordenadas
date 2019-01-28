cf6ff02e3dd96adafbba6ea0b1c799d0ed741b8b
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-01-27 23:36:54 -0600

    Guardado y Cargado con errores

--- simulacion/FormMain.Designer.cs
+++ simulacion/FormMain.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // panel1
@@ -42,10 +44,32 @@
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(876, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1005, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Cargar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // FormMain
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(1181, 773);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "FormMain";
@@ -57,8 +81,8 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-
-
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- simulacion/FormMain.cs
+++ simulacion/FormMain.cs
@@ -7,23 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace simulacion
 {
     public partial class FormMain : Form
     {
-        List<Coordenada> coordenadas;
+        List<Coordenada> listaCoordenadas;
+        const int sizeSquare = 40;
 
         public FormMain()
         {
             InitializeComponent();
-            coordenadas = new List<Coordenada>();
+            listaCoordenadas = new List<Coordenada>();
         }
 
-        private void DibujarLineas()
+        private void dibujarCuadricula()
         {
             int x, y, height, width, linesWidth, linesHeight;
-            const int sizeSquare = 40;
             Pen pen = new Pen(Color.Black, 2);
 
             height = panel1.Height;
@@ -48,7 +49,16 @@ namespace simulacion
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
-            DibujarLineas();
+            dibujarCuadricula();
+        }
+
+        private void dibujarCoordenada(Coordenada coordenada)
+        {
+            Pen pen = new Pen(Color.Red, 3);
+            float x, y;
+            x = coordenada.getX() * sizeSquare;
+            y = coordenada.getY() * sizeSquare;
+            panel1.CreateGraphics().DrawEllipse(pen, x, y, 5, 5);
         }
 
         private void panel1_MouseClick(object sender, MouseEventArgs e)
@@ -58,8 +68,8 @@ namespace simulacion
             coordenadaX = e.X;
             coordenadaY = e.Y;
 
-            coordenadaX = coordenadaX / 40;
-            coordenadaY = coordenadaY / 40;
+            coordenadaX = coordenadaX / sizeSquare;
+            coordenadaY = coordenadaY / sizeSquare;
 
             MessageBox.Show(coordenadaX + "," + coordenadaY);
 
@@ -68,13 +78,61 @@ namespace simulacion
             coordenada.setX(coordenadaX);
             coordenada.setY(coordenadaY);
 
-            coordenadas.Add(coordenada);
+            listaCoordenadas.Add(coordenada);
 
-            foreach (Coordenada coordenadaI in coordenadas)
+            foreach (Coordenada coordenadaI in listaCoordenadas)
             {
                 Console.WriteLine(coordenadaI.getX() + "  ,  " + coordenadaI.getY());
             }
             Console.WriteLine("------Coordenadas-----------");
+
+            dibujarCoordenada(coordenada);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter("coordenadas.txt"))
+            {
+                foreach (Coordenada coordenadaI in listaCoordenadas)
+                {
+                    sw.WriteLine(coordenadaI.getX() + "-" + coordenadaI.getY());
+                }
+                sw.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (StreamReader sr = new StreamReader("coordenadas.txt"))
+            {
+                string linea = "";
+                float x, y;
+                Coordenada coordenada = new Coordenada();
+                listaCoordenadas.Clear();
+
+                while (!sr.EndOfStream)
+                {
+                    string[] strCoordenada;
+
+                    linea = sr.ReadLine();
+                    strCoordenada = linea.Split('-');
+
+
+                    x = float.Parse(strCoordenada[0]);
+                    y = float.Parse(strCoordenada[1]);
+                    coordenada.setX(x);
+                    coordenada.setY(y);
+
+                    Console.WriteLine(coordenada.getX() + "  -  " + coordenada.getY());
+
+                    listaCoordenadas.Add(coordenada);
+
+                    dibujarCoordenada(coordenada);
+
+                }
+                sr.Close();
+            }
+            
         }
     }
 }
